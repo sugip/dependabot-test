7800445d2f8d3fdfec26904492995455cfb1b166
sugip <sugip952@gmail.com>
2026-02-15 13:49:04 +0900

    feat: add lodash

--- sample-function1/src/handlers/app.ts
+++ sample-function1/src/handlers/app.ts
@@ -1,6 +1,9 @@
+import _ from "lodash";
+
 export const lambdaHandler = async (): Promise<boolean> => {
 	try {
 		console.log("Hello, World! from sample-function1");
+		console.log(`random number: ${_.random()}`);
 
 		return true;
 	} catch (err) {
